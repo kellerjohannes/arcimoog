cdbc7fb0b61c91feadd3a3a08f62f1d2ae3d24c2
Johannes Keller <johannkell@gmail.com>
2024-02-12 17:16:18 +0100

    working on iterator implementation

--- lisp/pitch-data.lisp
+++ lisp/pitch-data.lisp
@@ -60,15 +60,22 @@ G♯. All other alterations are accepted as input but silently mapped onto these
   (:method-combination and :most-specific-last)
   (:documentation "Checks for validity of a pitch-data subclass."))
 
-(defgeneric iterator (pitch-data)
-  (:documentation "Returns a list with all possible pitch-data instances. This is only available for finite sets of
-pitch-data."))
+
+;; TODO: implement
+
+;; Challenge: some sort of range should be defined (start / end). The type of these arguments are
+;; used to dispatch the method correctly. The output should ideally be ordered according to sounding
+;; pitch, which requires the implementation of tonsystem and tuning system. Not clear how to deal
+;; with this.
+
+(defgeneric iterator (start end)
+  (:documentation "Returns an unordered list with all possible pitch-data instances between start and end."))
 
 
 
 (defmethod validp and ((note pitch-data))
   "Checks if the octave indicator is valid."
-  (log:info "Testing validity on PITCH-DATA level.")
+  (log:debug "Testing validity on PITCH-DATA level.")
   (or (null (octave note))
       (integerp (octave note))
       (and (listp (octave note))
@@ -79,7 +86,7 @@ pitch-data."))
 :g. ACCIDENTAL is only checked for types, not for specific keywords: it can be NIL, a single keyword
 or a list containing keywords (usually all identical, but not necessarily). Methods of subclasses
 are required to do the checking of accidentals."
-  (log:info "Testing validity on NOTE-NAME level.")
+  (log:debug "Testing validity on NOTE-NAME level.")
   (and (member (letter note) (list :a :b :c :d :e :f :g))
        (or (null (accidental note))
            (keywordp (accidental note))
@@ -100,16 +107,35 @@ are required to do the checking of accidentals."
 (defmethod validp and ((note note-name-smn))
   "Checks the validity for accidentals. They have to be :FLAT, :SHARP, NIL or a list containing
 uniquely :FLAT or :SHARP."
-  (log:info "Testing validity on NOTE-NAME-SMN level.")
+  (log:debug "Testing validity on NOTE-NAME-SMN level.")
   (let ((allowed-accidentals (list :sharp :flat))
         (candidate (if (listp (accidental note))
                        (reduce-accidental-list (accidental note))
                        (accidental note))))
     (member candidate allowed-accidentals)))
 
+(defmethod iterator ((start note-name-smn) (end note-name-smn) &optional (multiple-accidentals-depth 2))
+  (cond ((and (integerp (octave start))
+              (integerp (octave end)))
+         (log:warn "Octave indicators of START or END or both are not integers. Returning an empty nil.")
+         nil)
+        ((and (integerp multiple-accidentals-depth)
+              (>= multiple-accidentals-depth 0))
+         (log:warn "MULTIPLE-ACCIDENTALS-DEPTH" multiple-accidentals-depth "is invalid. Returning NIL.")))
+        (t (with-note-name-convention 'note-name-smn
+             (let ((result nil))
+               (loop for octave from (octave start) to (octave end)
+                     do (dolist (letter (list :a :b :c :d :e :f :g))
+                          (dolist (accidental (list nil :flat :sharp))
+                            (push (note letter accidental octave) result)
+                            (when (> multiple-accidentals-depth 1)
+                              (dotimes (i (1- multiple-accidentals-depth))
+                                (push (note letter accidental octave) result))))))
+               result))))
+
 (defmethod validp and ((note note-name-vicentino))
   "Checks the validity of accidentals and enharmonic shifts."
-  (log:info "Testing validity on NOTE-NAME-VICENTINO level.")
+  (log:debug "Testing validity on NOTE-NAME-VICENTINO level.")
   (let ((allowed-accidentals (list :sharp :flat))
         (allowed-enharmonic-alterations (list :dot :comma)))
     (and (or (null (accidental note))
@@ -122,17 +148,49 @@ uniquely :FLAT or :SHARP."
 
 (defmethod validp and ((note note-name-arciorgano))
   "Checks the validity of a note name on the Basel Arciorgano."
-  (log:info "Testing validity on NOTE-NAME-ARCIORGANO level.")
+  (log:debug "Testing validity on NOTE-NAME-ARCIORGANO level.")
   (and (< (octave note) 6)
        (> (octave note) 0))
   ;;TODO finish implementation
   )
 
 
+
+(defparameter *default-note-name-convention* 'note-name-vicentino)
+
+(defun set-default-note-name-convention (convention-class)
+  "Set the global default for the note naming convention, used to create new note name instances."
+  (if (member convention-class (list 'note-name-smn
+                                     'note-name-vicentino
+                                     'note-name-arciorgano
+                                     'key-name-vicentino
+                                     'key-name-arciorgano))
+      (setf *default-note-name-convention* convention-class)
+      (log:warn "Convention class unknown, DEFAULT-NOTE-NAME-CONVENTION will remain unchanged: "
+                *default-note-name-convention*)))
+
+;; TODO: write tests for this macro
+(defmacro with-note-name-convention (note-name-class &body body)
+  `(if (member ,note-name-class (list 'note-name-smn
+                                     'note-name-vicentino
+                                     'note-name-arciorgano
+                                     'key-name-vicentino
+                                     'key-name-arciorgano))
+       (let ((*default-note-name-convention* ,note-name-class))
+         ,@body)
+       (log:warn "Convention class unknown, DEFAULT-NOTE-NAME-CONVENTION will remain unchanged: "
+                 *default-note-name-convention*)))
+
+
+(defun note (letter alteration &optional (octave 2) enharmonic-dot)
+  (let ((result (make-instance *default-note-name-convention*)))
+    (setf (letter result) letter
+          (accidental result) alteration
+          (octave result) octave)
+    (when (member *default-note-name-convention* (list 'note-name-vicentino
+                                                       'note-name-arciorgano))
+      (setf (enharmonic-dot result) enharmonic-dot))
+    result))
+
 (defun test ()
-  (let ((note (make-instance 'note-name-arciorgano)))
-    (setf (octave note) 1
-          (letter note) :d
-          (accidental note) :sharp
-          (enharmonic-dot note) :comma)
-    (validp note)))
+  (validp (note :x nil)))
--- lisp/tests.lisp
+++ lisp/tests.lisp
@@ -7,18 +7,50 @@
 
 (5am:in-suite note-names)
 
+(5am:test setting-default-note-name-convention
+  :description "Testing the changing of the default note name convention. Needs to be updated whenever new convention classes are defined."
 
-(5am:test vicentino-note-names
-  (let ((note1 (make-instance 'note-name-vicentino))
-        (note2 (make-instance 'note-name-vicentino)))
-    (setf (octave note1) 1
-          (accidental note1) nil
-          (letter note1) :d
-          (enharmonic-dot note1) :dot)
-    (setf (octave note2) 1
-          (accidental note2) :nothing
-          (letter note2) :d
-          (enharmonic-dot note2) nil)
-    (log:info "test" (validp note2))
-    (5am:is (validp note1) t)
-    (5am:is (validp note2) nil)))
+  (set-default-note-name-convention 'note-name-smn)
+  (5am:is (eq *default-note-name-convention* 'note-name-smn))
+
+  (set-default-note-name-convention 'note-name-vicentino)
+  (5am:is (eq *default-note-name-convention* 'note-name-vicentino))
+
+  (set-default-note-name-convention 'note-name-arciorgano)
+  (5am:is (eq *default-note-name-convention* 'note-name-arciorgano))
+
+  (set-default-note-name-convention 'key-name-vicentino)
+  (5am:is (eq *default-note-name-convention* 'key-name-vicentino))
+
+  (set-default-note-name-convention 'key-name-arciorgano)
+  (5am:is (eq *default-note-name-convention* 'key-name-arciorgano)))
+
+(5am:test note-name-creation
+  :description "Testing the function NOTE."
+  (set-default-note-name-convention 'note-name-smn)
+  (loop for letter in (list :a :b :c :d :e :f :g)
+        do (loop for accidental in (list nil :sharp :flat)
+                 do (loop for octave from -3 to 8
+                          do (let ((n-oct (note letter accidental octave))
+                                   (n-pc (note letter accidental)))
+                               (5am:is (letter n-oct) letter)
+                               (5am:is (letter n-pc) letter)
+                               (5am:is (eq (accidental n-oct) accidental))
+                               (5am:is (eq (accidental n-pc) accidental))
+                               (5am:is (= (octave n-oct) octave))
+                               (5am:is (= (octave n-pc) 2)))))))
+
+;; (5am:test vicentino-note-names
+;;   (let ((note1 (make-instance 'note-name-vicentino))
+;;         (note2 (make-instance 'note-name-vicentino)))
+;;     (setf (octave note1) 1
+;;           (accidental note1) nil
+;;           (letter note1) :d
+;;           (enharmonic-dot note1) :dot)
+;;     (setf (octave note2) 1
+;;           (accidental note2) :nothing
+;;           (letter note2) :d
+;;           (enharmonic-dot note2) nil)
+;;     (log:info "test" (validp note2))
+;;     (5am:is (validp note1) t)
+;;     (5am:is (validp note2) nil)))
