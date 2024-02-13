0f65dbcb4c1ac7d72744dd2815d98ba1a392d933
Johannes Keller <johannkell@gmail.com>
2024-02-13 17:35:17 +0100

    implementing print objects

--- lisp/pitch-data.lisp
+++ lisp/pitch-data.lisp
@@ -1,76 +1,44 @@
 (in-package :arcimoog)
 
+;; TODO
+;; Allowed accidentals etc.: define as global parameters for each class
+
+
+
+;;;;;;;;;;;;;;;
+;; Utilities ;;
+;;;;;;;;;;;;;;;
+
+(defun reduce-accidental-list (accidental-list)
+  "Excepts a list of identical keywords"
+  (let ((simple-list (remove-duplicates accidental-list)))
+    (cond ((= 1 (length simple-list))
+           (cond ((keywordp (car simple-list))
+                  (car simple-list))
+                 (t (log:warn "The simplified ACCIDENTAL-LIST" accidental-list "doesn't consist of a keyword and is therefore ignored. This might produce unwanted results.")
+                    nil)))
+          (t (log:warn "The ACCIDENTAL-LIST" accidental-list "doesn't consist of exclusively identical keywords and will therefore be ignored. This might produce unwanted results.")
+             nil))))
+
+
+;;;;;;;;;;;;;;;;
+;; PITCH-DATA ;;
+;;;;;;;;;;;;;;;;
+
+;; Parent class for all kinds of pitch data, from the beginning to the end of the pitch
+;; pipeline. The OCTAVE slot is always present.
+
 (defclass pitch-data ()
   ((octave :accessor octave
            :documentation "NIL: treated as pitch class, NUMBER: octave indicator, LIST of NUMBERs: simultaneous octaves"))
   (:documentation "Parent class of subclasses for any kind of pitch data."))
 
-(defclass note-name (pitch-data)
-  ((letter :accessor letter
-           :documentation "Keyword describing the root note name.")
-   (accidental :accessor accidental
-               :documentation "KEYWORD describing an accidental, NIL for the raw root name."))
-  (:documentation "Parent class for note names based on letters and alterations."))
-
-(defclass note-name-smn (note-name)
-  ()
-  (:documentation "Operational subclass for note names that are part of standard music notation (multiple alterations
-are accepted)."))
-
-(defclass note-name-12 (note-name-smn)
-  ()
-  (:documentation "Operational subclass for note names represented by a standard keyboard with B♭, E♭ and F♯, C♯,
-G♯. All other alterations are accepted as input but silently mapped onto these note names."))
-
-(defclass note-name-vicentino (note-name)
-  ((enharmonic-dot :accessor enharmonic-dot
-                   :documentation "NIL if absent, :dot when raised by a 'diesis enarmonico minore', :comma when raised by a 'comma'."))
-  (:documentation "Operational subclass for note names of Vicentino's staff notation."))
-
-(defclass note-name-arciorgano (note-name-vicentino)
-  ()
-  (:documentation "Operational subclass representing the available notes of the Basel Arciorgano."))
-
-(defclass key-name-vicentino (pitch-data)
-  ((letter :accessor letter
-           :documentation "Keyword :a-:g.")
-   (ordine :accessor ordine
-           :documentation "NUMBER 1-6."))
-  (:documentation "Operational subclass for key names according to Vicentinos L'antica musica (Rome 1555)."))
-
-(defclass key-name-arciorgano (key-name-vicentino)
-  ()
-  (:documentation "Operational subclass representing the available keys of the Basel Arciorgano."))
-
-
-
-
-
-;; (defmethod least-specific-first-combination (methods generic-function)
-;;   (destructuring-bind (&key least-specific first-class next-methods)
-;;       methods
-;;     (funcall least-specific)))
-
-;; (defgeneric my-generic-function ()
-;;   (:method-combination least-specific-first-combination))
-
-
-
 (defgeneric validp (pitch-data)
   (:method-combination and :most-specific-last)
-  (:documentation "Checks for validity of a pitch-data subclass."))
-
-
-;; TODO: implement
-
-;; Challenge: some sort of range should be defined (start / end). The type of these arguments are
-;; used to dispatch the method correctly. The output should ideally be ordered according to sounding
-;; pitch, which requires the implementation of tonsystem and tuning system. Not clear how to deal
-;; with this.
+  (:documentation "Checks for validity of a pitch-data subclass. Starts with least specific method in order to test for the rough things first, and get more and more refined throughout the dispatch chain."))
 
 (defgeneric iterator (start end)
-  (:documentation "Returns an unordered list with all possible pitch-data instances between start and end."))
-
+  (:documentation "Returns an unordered list with all possible PITCH-DATA instances between start and end. Since in many cases (for example NOTE-NAME) the pitch is unknown, the list is not sorted."))
 
 
 (defmethod validp and ((note pitch-data))
@@ -80,6 +48,20 @@ G♯. All other alterations are accepted as input but silently mapped onto these
       (integerp (octave note))
       (and (listp (octave note))
            (every #'integerp (octave note)))))
+
+
+;;;;;;;;;;;;;;;
+;; NOTE-NAME ;;
+;;;;;;;;;;;;;;;
+
+;; Note names based on letter/accidental.
+
+(defclass note-name (pitch-data)
+  ((letter :accessor letter
+           :documentation "Keyword describing the root note name.")
+   (accidental :accessor accidental
+               :documentation "KEYWORD describing an accidental, NIL for the raw root name."))
+  (:documentation "Parent class for note names based on letters and alterations."))
 
 (defmethod validp and ((note note-name))
   "Checks for validity of generic note name. The letter needs to be a keyword between :a and
@@ -93,16 +75,36 @@ are required to do the checking of accidentals."
            (and (listp (accidental note))
                 (every #'keywordp (accidental note))))))
 
-(defun reduce-accidental-list (accidental-list)
-  "Excepts a list of identical keywords"
-  (let ((simple-list (remove-duplicates accidental-list)))
-    (cond ((= 1 (length simple-list))
-           (cond ((keywordp (car simple-list))
-                  (car simple-list))
-                 (t (log:warn "The simplified ACCIDENTAL-LIST" accidental-list "doesn't consist of a keyword and is therefore ignored. This might produce unwanted results.")
-                    nil)))
-          (t (log:warn "The ACCIDENTAL-LIST" accidental-list "doesn't consist of exclusively identical keywords and will therefore be ignored. This might produce unwanted results.")
-             nil))))
+
+;;;;;;;;;;;;;;;;;;;
+;; NOTE-NAME-SMN ;;
+;;;;;;;;;;;;;;;;;;;
+
+;; Note names in standard music notation with single and multiple accidentals.
+
+(defclass note-name-smn (note-name)
+  ()
+  (:documentation "Operational subclass for note names that are part of standard music notation (multiple alterations
+are accepted)."))
+
+(defun print-smn-accidental (accidental)
+  (cond ((null accidental) nil)
+        ((keywordp accidental)
+         (ecase accidental
+           (:sharp "♯")
+           (:flat "♭")))
+        ((listp accidental)
+         (make-string (length accidental)
+                      :initial-element (ecase (first accidental)
+                                         (:sharp #\SHARP)
+                                         (:flat #\FLAT))))))
+
+(defmethod print-object ((note note-name-smn) out)
+  (print-unreadable-object (note out :type t)
+    (format out "~a~@[~a~][~a]"
+            (letter note)
+            (print-smn-accidental (accidental note))
+            (octave note))))
 
 (defmethod validp and ((note note-name-smn))
   "Checks the validity for accidentals. They have to be :FLAT, :SHARP, NIL or a list containing
@@ -114,14 +116,15 @@ uniquely :FLAT or :SHARP."
                        (accidental note))))
     (member candidate allowed-accidentals)))
 
-(defmethod iterator ((start note-name-smn) (end note-name-smn) &optional (multiple-accidentals-depth 2))
+;; TODO: finish implementation, and implement others
+(defmethod iterator ((start note-name-smn) (end note-name-smn))
   (cond ((and (integerp (octave start))
               (integerp (octave end)))
          (log:warn "Octave indicators of START or END or both are not integers. Returning an empty nil.")
          nil)
         ((and (integerp multiple-accidentals-depth)
               (>= multiple-accidentals-depth 0))
-         (log:warn "MULTIPLE-ACCIDENTALS-DEPTH" multiple-accidentals-depth "is invalid. Returning NIL.")))
+         (log:warn "MULTIPLE-ACCIDENTALS-DEPTH" multiple-accidentals-depth "is invalid. Returning NIL."))
         (t (with-note-name-convention 'note-name-smn
              (let ((result nil))
                (loop for octave from (octave start) to (octave end)
@@ -131,7 +134,64 @@ uniquely :FLAT or :SHARP."
                             (when (> multiple-accidentals-depth 1)
                               (dotimes (i (1- multiple-accidentals-depth))
                                 (push (note letter accidental octave) result))))))
-               result))))
+               result)))))
+
+
+;;;;;;;;;;;;;;;;;;
+;; NOTE-NAME-12 ;;
+;;;;;;;;;;;;;;;;;;
+
+;; Note names, projected to a keyboard with C♯, E♭, F♯, G♯ and B♭. When importing, enharmonics will
+;; be applied.
+
+(defclass note-name-12 (note-name-smn)
+  ()
+  (:documentation "Operational subclass for note names represented by a standard keyboard with B♭, E♭ and F♯, C♯,
+G♯. All other alterations are accepted as input but silently mapped onto these note names."))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; NOTE-NAME-VICENTINO ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Note names based on Nicola Vicentinos convention, where every conventional note name can be
+;; further altered with an enharmonic dot or a comma (or both).
+
+(defclass note-name-vicentino (note-name)
+  ((enharmonic-dot :accessor enharmonic-dot
+                   :documentation "NIL if absent, :dot when raised by a 'diesis enarmonico minore', :comma when raised by a 'comma'."))
+  (:documentation "Operational subclass for note names of Vicentino's staff notation."))
+
+(defparameter *vicentino-dotted-letters*
+  '((:a "Ȧ" #\LATIN_CAPITAL_LETTER_A_WITH_DOT_ABOVE)
+    (:b "Ḃ" #\LATIN_CAPITAL_LETTER_B_WITH_DOT_ABOVE)
+    (:c "Ċ" #\LATIN_CAPITAL_LETTER_C_WITH_DOT_ABOVE)
+    (:d "Ḋ" #\LATIN_CAPITAL_LETTER_D_WITH_DOT_ABOVE)
+    (:e "Ė" #\LATIN_CAPITAL_LETTER_E_WITH_DOT_ABOVE)
+    (:f "Ḟ" #\LATIN_CAPITAL_LETTER_F_WITH_DOT_ABOVE)
+    (:g "Ġ" #\LATIN_CAPITAL_LETTER_G_WITH_DOT_ABOVE)))
+
+
+(defun lookup-vicentino-letter-string (letter-keyword)
+  (second (assoc letter-keyword *vicentino-dotted-letters*)))
+
+(defun lookup-vicentino-letter-char (letter-keyword)
+  (third (assoc letter-keyword *vicentino-dotted-letters*)))
+
+(defmethod print-object ((note note-name-vicentino) out)
+  (print-unreadable-object (note out :type t)
+    (format out "~a~@[~a~]~@[~a~][~a]"
+            (if (or (eq (enharmonic-dot note) :dot)
+                    (and (listp (enharmonic-dot note))
+                         (member :dot (enharmonic-dot note))))
+                (lookup-vicentino-letter-string (letter note))
+                (letter note))
+            (print-smn-accidental (accidental note))
+            (when (or (eq (enharmonic-dot note) :comma)
+                      (and (listp (enharmonic-dot note))
+                           (member :comma (enharmonic-dot note))))
+              "❜")
+            (octave note))))
 
 (defmethod validp and ((note note-name-vicentino))
   "Checks the validity of accidentals and enharmonic shifts."
@@ -146,6 +206,18 @@ uniquely :FLAT or :SHARP."
                   (and (subsetp (enharmonic-dot note) allowed-enharmonic-alterations)
                        (subsetp allowed-enharmonic-alterations (enharmonic-dot note))))))))
 
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; NOTE-NAME-ARCIORGANO ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Note names that are available on the Basel reconstruction of Vicentino's Arciorgano (Fleig 2016).
+
+(defclass note-name-arciorgano (note-name-vicentino)
+  ()
+  (:documentation "Operational subclass representing the available notes of the Basel Arciorgano."))
+
 (defmethod validp and ((note note-name-arciorgano))
   "Checks the validity of a note name on the Basel Arciorgano."
   (log:debug "Testing validity on NOTE-NAME-ARCIORGANO level.")
@@ -155,8 +227,47 @@ uniquely :FLAT or :SHARP."
   )
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;
+;; KEY-NAME-VICENTINO ;;
+;;;;;;;;;;;;;;;;;;;;;;;;
 
-(defparameter *default-note-name-convention* 'note-name-vicentino)
+;; Key names based on Nicola Vicentino's convention, using the letters :a - :g and the indices 1-6.
+
+(defclass key-name-vicentino (pitch-data)
+  ((letter :accessor letter
+           :documentation "Keyword :a-:g.")
+   (ordine :accessor ordine
+           :documentation "NUMBER 1-6."))
+  (:documentation "Operational subclass for key names according to Vicentinos L'antica musica (Rome 1555)."))
+
+;;;;;;;;;;;;;;;;;;;;;;;;;
+;; KEY-NAME-ARCIORGANO ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; Key names based on the Basel reconstruction of Vicentino's Arciorgano (Fleig 2016).
+
+(defclass key-name-arciorgano (key-name-vicentino)
+  ()
+  (:documentation "Operational subclass representing the available keys of the Basel Arciorgano."))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+(defparameter *default-note-name-convention* 'note-name-smn)
 
 (defun set-default-note-name-convention (convention-class)
   "Set the global default for the note naming convention, used to create new note name instances."
@@ -169,20 +280,24 @@ uniquely :FLAT or :SHARP."
       (log:warn "Convention class unknown, DEFAULT-NOTE-NAME-CONVENTION will remain unchanged: "
                 *default-note-name-convention*)))
 
-;; TODO: write tests for this macro
+;; TODO: write 5am-tests for this macro
 (defmacro with-note-name-convention (note-name-class &body body)
+  "Dynamically rebind the global symbol *default-note-name-convention*."
   `(if (member ,note-name-class (list 'note-name-smn
-                                     'note-name-vicentino
-                                     'note-name-arciorgano
-                                     'key-name-vicentino
-                                     'key-name-arciorgano))
+                                      'note-name-12
+                                      'note-name-vicentino
+                                      'note-name-arciorgano
+                                      'key-name-vicentino
+                                      'key-name-arciorgano))
        (let ((*default-note-name-convention* ,note-name-class))
          ,@body)
-       (log:warn "Convention class unknown, DEFAULT-NOTE-NAME-CONVENTION will remain unchanged: "
-                 *default-note-name-convention*)))
+       (progn
+         (log:warn "Convention class unknown, DEFAULT-NOTE-NAME-CONVENTION will remain unchanged: "
+                   *default-note-name-convention*)
+         ,@body)))
 
-
-(defun note (letter alteration &optional (octave 2) enharmonic-dot)
+(defun note (letter &optional (alteration nil) (octave 2) enharmonic-dot)
+  "Create an instance of a PITCH-DATA subclass."
   (let ((result (make-instance *default-note-name-convention*)))
     (setf (letter result) letter
           (accidental result) alteration
@@ -190,7 +305,6 @@ uniquely :FLAT or :SHARP."
     (when (member *default-note-name-convention* (list 'note-name-vicentino
                                                        'note-name-arciorgano))
       (setf (enharmonic-dot result) enharmonic-dot))
+    (unless (validp result)
+      (log:warn "Validity for newly created PITCH-INFO" result "failed. This might create unwanted results. The PITCH INFO instance is returend nevertheless."))
     result))
-
-(defun test ()
-  (validp (note :x nil)))
