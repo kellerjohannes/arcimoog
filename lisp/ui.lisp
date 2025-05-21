7ff8ddb55953361134c0d86aea5b90e73263f5d3
Johannes Keller <johannkell@gmail.com>
2025-05-21 13:20:24 +0200

    tried many things, still flickering

--- lisp/ui.lisp
+++ lisp/ui.lisp
@@ -85,7 +85,6 @@
     program))
 
 
-
 (defclass roll (gl-object)
   ((color :initform (list 1.0 1.0 1.0) :accessor color)
    (name :initarg :name :accessor name)))
@@ -96,48 +95,26 @@
                             x y z w))
 
 (defmethod draw ((obj roll))
+  ;;(format t "~&Drawing roll ~a.~%" (name obj))
   (use-program (program obj))
   (bind-vertex-array (vao obj))
   (bind-buffer (vbo obj) :ARRAY_BUFFER)
-
-  ;; (macrolet ((draw-tracker (name r g b)
-  ;;              `(progn
-  ;;                 (set-uniform-f obj "color" ,r ,g ,b)
-  ;;                 (when (am-ht:update-data-required-p ,name)
-  ;;                   ;; (format t "~&buffer updated.")
-  ;;                   (buffer-data (vbo obj)
-  ;;                                           (am-ht:dump-gl-list ,name)
-  ;;                                           "Float32Array"
-  ;;                                           :STATIC_DRAW)
-  ;;                   (am-ht:data-updated ,name))
-  ;;                 (draw-arrays (webgl obj)
-  ;;                                         :LINE_STRIP
-  ;;                                         0
-  ;;                                         (floor (am-ht:length-gl-data ,name) 2)))))
-
-  ;;   (set-uniform-f obj "xOffset" (am-par:get-scalar :cv-history-x-offset))
-  ;;   (set-uniform-f obj "xFactor" (am-par:get-scalar :cv-history-x-scale))
-  ;;   (set-uniform-f obj "yFactor" (am-par:get-scalar :cv-history-y-scale))
-
-  ;;   ;; (format t "~&hi ~a" (name obj))
-
-  ;;   (draw-tracker (name obj) 1.0 1.0 0.0))
 
 
   ;; (set-uniform-f obj "xOffset" (am-par:get-scalar :cv-history-x-offset))
   ;; (set-uniform-f obj "xFactor" (am-par:get-scalar :cv-history-x-scale))
   ;; (set-uniform-f obj "yFactor" (am-par:get-scalar :cv-history-y-scale))
   (set-uniform-f obj "xOffset" 0.0)
-  (set-uniform-f obj "xFactor" 0.1)
+  (set-uniform-f obj "xFactor" 1.0)
   (set-uniform-f obj "yFactor" 1.0)
   (set-uniform-f obj "color" (first (color obj)) (second (color obj)) (third (color obj)))
-  ;; (when t ;(am-ht:update-data-required-p (name obj))
-  ;;   (buffer-data (vbo obj)
-  ;;                           ;;(am-ht:dump-gl-list (name obj))
-  ;;                           (list 0.0 0.0 1.0 1.0)
-  ;;                           "Float32Array"
-  ;;                           :STATIC_DRAW)
-  ;;   (am-ht:data-updated (name obj)))
+  (when t ;(am-ht:update-data-required-p (name obj))
+    (buffer-data (vbo obj)
+                            ;;(am-ht:dump-gl-list (name obj))
+                            (list 0.0 0.0 1.0 1.0)
+                            "Float32Array"
+                            :STATIC_DRAW)
+    (am-ht:data-updated (name obj)))
   (draw-arrays (webgl obj)
                           :LINE_STRIP
                           0
@@ -146,7 +123,6 @@
                           )
 
   ;; (format t "~&~a" (webgl-error (webgl obj)))
-  ;; (format t "~&hi ~a" (name obj))
   )
 
 
@@ -161,12 +137,10 @@ uniform float yFactor;
 uniform float xOffset;
 
 void main() {
-  Color = vec3(1.0, 0.0, 1.0);
-  gl_Position = vec4(position.x, position.y, 0.0, 1.0);
+ Color = color;
+ gl_Position = vec4(xOffset + xFactor * position.x, yFactor * position.y, 0.0, 1.0);
 }")
 
-;; Color = color;
-;; gl_Position = vec4(xOffset + xFactor * position.x, yFactor * position.y, 0.0, 1.0);
 
 (defparameter *rolls-f-shader* "#version 300 es
 precision highp float;
@@ -198,40 +172,11 @@ void main() {
 
 (defparameter *frame-rate* (/ 1000 2))
 
-(defun animation-handler (obj time-string)
-  (let ((time (incudine.util:parse-float time-string)))
-    (when (> (- time (connection-data-item obj "previous-time"))
-             *frame-rate*)
-      (format t "~&~a" time)
-      (clear-webgl (connection-data-item obj "gl-object") :COLOR_BUFFER_BIT)
-      (let ((roll *rolls* ;(first (connection-data-item obj "rolls"))
-              ))
-
-        ;; (dolist (roll (connection-data-item obj "rolls"))
-        ;;   (draw roll))
-        ;;       (draw (first (connection-data-item obj "rolls")))
-
-
-
-        (use-program (program roll))
-        (bind-vertex-array (vao roll))
-;;        (bind-buffer (vbo roll) :ARRAY_BUFFER)
-
-        ;; (set-uniform-f roll "xOffset" 0.0)
-        ;; (set-uniform-f roll "xFactor" 0.1)
-        ;; (set-uniform-f roll "yFactor" 1.0)
-        ;; (set-uniform-f roll "color"
-        ;;                (first (color roll)) (second (color roll)) (third (color roll)))
-
-        (draw-arrays (webgl roll)
-                                :LINE_STRIP
-                                0
-                                ;;(floor (am-ht:length-gl-data (name obj)) 2)
-                                2
-                                ))
-
-      (setf (connection-data-item obj "previous-time") time))
-    (request-animation-frame (connection-data-item obj "window"))))
+(defun draw-rolls (clog-obj rolls)
+  ;; (format t "~&Drawing.~%")
+  (clear-webgl (connection-data-item clog-obj "gl-object") :COLOR_BUFFER_BIT)
+  (dolist (roll rolls)
+    (draw roll)))
 
 (defparameter *rolls* nil)
 
@@ -242,28 +187,24 @@ void main() {
          (gl (create-webgl canvas :attributes '("preserveDrawingBuffer" t
                                                            "powerPreference" "low-power"
                                                            "antialias" t)))
-         (rolls (list nil ;;(make-roll gl :vco1 (list 1.0 0.0 0.0))
-                      ;;(make-roll gl :vcf1 (list 0.0 1.0 0.0))
-                      ;;(make-roll gl :res1 (list 0.0 0.0 1.0))
+         (rolls (list (make-roll gl :vco1 (list 1.0 0.0 0.0))
+      ;;                (make-roll gl :vcf1 (list 0.0 1.0 0.0))
+      ;;                (make-roll gl :res1 (list 1.0 1.0 0.0))
                       )))
     (setf (connection-data-item clog-parent "gl-object") gl)
     (setf (connection-data-item clog-parent "rolls") rolls)
     (setf (connection-data-item clog-parent "previous-time") 0)
-
-    (setf *rolls* (make-roll gl :vco1 (list 1.0 0.0 0.0)))
-
-    (set-on-animation-frame (connection-data-item clog-parent "window")
-                                 #'animation-handler)
     (set-border canvas :medium :solid :green)
     (enable-capability gl :BLEND)
     (disable-capability gl :DEPTH_TEST)
     (blend-function gl :ONE :ONE_MINUS_SRC_ALPHA)
     (clear-color gl 0.0f0 0.0f0 0.0f0 1.0f0)
     (clear-depth gl 1)
-    (clear-webgl gl :COLOR_BUFFER_BIT)
     (viewport gl 0 0 1200 700)
-    (request-animation-frame (connection-data-item clog-parent "window"))
-    ))
+    (bt:make-thread (lambda ()
+                      (loop
+                        (draw-rolls clog-parent rolls)
+                        (sleep 1/5))))))
 
 
 
@@ -276,8 +217,7 @@ void main() {
 (defun build-ui (parent)
   (create-div parent :class "main-title" :content "Arcimoog")
   (build-cv-meters (create-div parent :class "tile"))
-  (build-cv-roll (create-div parent :class "tile"))
-  )
+  (build-cv-roll (create-div parent :class "tile")))
 
 (defun on-main (body)
   (load-css (html-document body) "styles.css")
